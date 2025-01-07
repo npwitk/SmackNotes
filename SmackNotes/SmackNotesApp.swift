1a1e62049fb152e20b23f80fbafad7c1d11d7516
Nonprawich I <40120130+npwitk@users.noreply.github.com>
2025-01-07 10:09:42 +0700

    Initial Commit

---
+++ SmackNotes/SmackNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmackNotesApp.swift
+//  SmackNotes
+//
+//  Created by Nonprawich I. on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmackNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
